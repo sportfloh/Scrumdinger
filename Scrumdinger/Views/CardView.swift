510931b84b3e6a8945c5898352fc551ee06029a5
Florian Bruder <florian.bruder@posteo.de>
2022-01-27 21:14:39 +0100

    Implement CardView

--- Scrumdinger/Views/CardView.swift
+++ Scrumdinger/Views/CardView.swift
@@ -10,15 +10,35 @@ import SwiftUI
 // MARK: -
 
 struct CardView: View {
+    let scrum: DailyScrum
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .labelStyle(.trailingIcon)
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
     }
 }
 
 // MARK: -
 
 struct CardView_Previews: PreviewProvider {
+    static let scrum = DailyScrum.sampleData[0]
+
     static var previews: some View {
-        CardView()
+        CardView(scrum: scrum)
+            .background(.yellow)
+            .previewLayout(.fixed(width: 400, height: 60))
     }
 }
