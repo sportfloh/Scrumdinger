f1cd562b1fb8d8d890561c8c6133fe786333ca65
Florian Bruder <florian.bruder@posteo.de>
2022-01-27 20:52:53 +0100

    Better sampleData formatting

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -20,8 +20,20 @@ struct DailyScrum {
 
 extension DailyScrum {
     static let sampleData: [DailyScrum] = [
-        DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, theme: .yellow),
-        DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
-        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
+        DailyScrum(
+            title: "Design",
+            attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+            lengthInMinutes: 10,
+            theme: .yellow),
+        DailyScrum(
+            title: "App Dev",
+            attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+            lengthInMinutes: 5,
+            theme: .orange),
+        DailyScrum(
+            title: "Web Dev",
+            attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
+            lengthInMinutes: 5,
+            theme: .poppy)
     ]
 }
