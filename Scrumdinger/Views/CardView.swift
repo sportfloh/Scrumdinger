0e0349b81fd8a68168bd4eb840dd95a759744725
Florian Bruder <florian.bruder@posteo.de>
2022-01-27 20:33:31 +0100

    Add CardView.swift

---
+++ Scrumdinger/Views/CardView.swift
@@ -0,0 +1,24 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Florian Bruder on 27.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
