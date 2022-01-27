fc7bd44d4e623a3fa15dced5ff91598be0b1ea4f
Florian Bruder <florian.bruder@posteo.de>
2022-01-27 23:13:13 +0100

    Add test for Theme type

--- ScrumdingerTests/ThemeTests.swift
+++ ScrumdingerTests/ThemeTests.swift
@@ -5,8 +5,19 @@
 //  Created by Florian Bruder on 27.01.22.
 //
 
+@testable import Scrumdinger
+import SwiftUI
 import XCTest
 
 // MARK: -
 
-final class ThemeTests: XCTestCase {}
+final class ThemeTestCase: XCTestCase {}
+
+// MARK: - test mainColor property
+
+extension ThemeTestCase {
+    func testMainColorBundleValue() {
+        let theme = Theme.yellow
+        XCTAssertEqual(Color("yellow"), theme.mainColor)
+    }
+}
