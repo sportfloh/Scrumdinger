22a827b36bea7ef3bf8c786d0717200c926a97c2
Florian Bruder <florian.bruder@posteo.de>
2022-01-27 21:33:54 +0100

    Add ScrumsView.swift

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,24 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Florian Bruder on 27.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView()
+    }
+}
