1a979925d366ff3f957823d4cf181458bbc469aa
Florian Bruder <florian.bruder@posteo.de>
2022-01-30 00:22:14 +0100

    Add details to DetailView

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -13,7 +13,34 @@ struct DetailView: View {
     let scrum: DailyScrum
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section(
+                header: Text("Meeting Info"),
+                content: {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+
+                    HStack {
+                        Label("Length", systemImage: "clock")
+                        Spacer()
+                        Text("\(scrum.lengthInMinutes) minutes")
+                    }
+                    .accessibilityElement(children: .combine)
+
+                    HStack {
+                        Label("Theme", systemImage: "paintpalette")
+                        Spacer()
+                        Text(scrum.theme.name)
+                            .padding(4)
+                            .foregroundColor(scrum.theme.accentColor)
+                            .background(scrum.theme.mainColor)
+                            .cornerRadius(4)
+                    }
+                    .accessibilityElement(children: .combine)
+                }
+            )
+        }
     }
 }
 
