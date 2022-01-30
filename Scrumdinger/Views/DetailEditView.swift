5ef8c8934508da865100d0cb378eb485f4cb7937
Florian Bruder <florian.bruder@posteo.de>
2022-01-30 18:03:06 +0100

    Add a title TextField and a length Slider to DetailEditView

--- Scrumdinger/Views/DetailEditView.swift
+++ Scrumdinger/Views/DetailEditView.swift
@@ -10,8 +10,21 @@ import SwiftUI
 // MARK: -
 
 struct DetailEditView: View {
+    @State private var data = DailyScrum.Data()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $data.title)
+                HStack {
+                    Slider(value: $data.lengthInMinutes, in: 5 ... 30, step: 1) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(Int(data.lengthInMinutes)) minutes")
+                }
+            }
+        }
     }
 }
 
