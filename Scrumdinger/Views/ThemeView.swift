e63d71a8a5f8bfc82f22f9126640e1473a4422f3
Florian Bruder <florian.bruder@posteo.de>
2022-01-30 19:23:20 +0100

    Add ThemeView.swift

---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,24 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Florian Bruder on 30.01.22.
+//
+
+import SwiftUI
+
+// MARK: - -
+
+struct ThemeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - -
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView()
+    }
+}
