53116546257d08aa9322ea953ce1c5358e55748e
Florian Bruder <florian.bruder@posteo.de>
2022-01-30 15:34:41 +0100

    Add list of attendees to DailyScrum

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -14,32 +14,34 @@ struct DetailView: View {
 
     var body: some View {
         List {
-            Section(
-                header: Text("Meeting Info"),
-                content: {
-                    Label("Start Meeting", systemImage: "timer")
-                        .font(.headline)
-                        .foregroundColor(.accentColor)
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
 
-                    HStack {
-                        Label("Length", systemImage: "clock")
-                        Spacer()
-                        Text("\(scrum.lengthInMinutes) minutes")
-                    }
-                    .accessibilityElement(children: .combine)
-
-                    HStack {
-                        Label("Theme", systemImage: "paintpalette")
-                        Spacer()
-                        Text(scrum.theme.name)
-                            .padding(4)
-                            .foregroundColor(scrum.theme.accentColor)
-                            .background(scrum.theme.mainColor)
-                            .cornerRadius(4)
-                    }
-                    .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
                 }
-            )
+                .accessibilityElement(children: .combine)
+
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
         }
     }
 }
