a95813a29d3010af1e8589c9e6c4887ebf8b42ee
Florian Bruder <florian.bruder@posteo.de>
2022-01-30 18:28:26 +0100

    Add attendees section to DetailEditView

--- Scrumdinger/Views/DetailEditView.swift
+++ Scrumdinger/Views/DetailEditView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 
 struct DetailEditView: View {
     @State private var data = DailyScrum.Data()
+    @State private var newAttendeeName = ""
 
     var body: some View {
         Form {
@@ -22,6 +23,31 @@ struct DetailEditView: View {
                     }
                     Spacer()
                     Text("\(Int(data.lengthInMinutes)) minutes")
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(data.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    data.attendees.remove(atOffsets: indices)
+                }
+
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(
+                        action: {
+                            withAnimation {
+                                let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                                data.attendees.append(attendee)
+                                newAttendeeName = ""
+                            }
+                        },
+                        label: {
+                            Image(systemName: "plus.circle.fill")
+                        }
+                    )
+                    .disabled(newAttendeeName.isEmpty)
                 }
             }
         }
