31441b44bc88d3511ce2dc5c879155dc871f000d
Florian Bruder <florian.bruder@posteo.de>
2022-01-30 19:56:21 +0100

    Implement ThemeView

--- Scrumdinger/Views/ThemeView.swift
+++ Scrumdinger/Views/ThemeView.swift
@@ -7,18 +7,28 @@
 
 import SwiftUI
 
-// MARK: - -
+// MARK: -
 
 struct ThemeView: View {
+    let theme: Theme
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 4)
+                .fill(theme.mainColor)
+
+            Label(theme.name, systemImage: "paintpalette")
+                .padding(4)
+        }
+        .foregroundColor(theme.accentColor)
+        .fixedSize(horizontal: false, vertical: true)
     }
 }
 
-// MARK: - -
+// MARK: -
 
 struct ThemeView_Previews: PreviewProvider {
     static var previews: some View {
-        ThemeView()
+        ThemeView(theme: .buttercup)
     }
 }
