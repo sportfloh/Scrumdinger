00cf4097fc39eac80562492e6a407c94eaeea042
Florian Bruder <florian.bruder@posteo.de>
2022-01-30 17:42:14 +0100

    Add DetailEditView.swift

---
+++ Scrumdinger/Views/DetailEditView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Florian Bruder on 30.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
