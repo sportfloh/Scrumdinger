eb7cb948f4721ed00ebb3c497107ea76975b1722
Florian Bruder <florian.bruder@posteo.de>
2022-01-31 20:12:44 +0100

    Add MeetingHeaderView.swift and MeetingView.swift to MeetingView group

---
+++ Scrumdinger/Views/MeetingView/MeetingHeaderView.swift
@@ -0,0 +1,24 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by Florian Bruder on 31.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct MeetingHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct MeetingHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingHeaderView()
+    }
+}
