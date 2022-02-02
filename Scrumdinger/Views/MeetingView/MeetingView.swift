51265ab5d6df3aa336c22206634fde645b303842
Florian Bruder <florian.bruder@posteo.de>
2022-02-02 21:55:26 +0100

    Extract MeetingFooterView into separat view

--- Scrumdinger/Views/MeetingView/MeetingView.swift
+++ Scrumdinger/Views/MeetingView/MeetingView.swift
@@ -27,16 +27,9 @@ struct MeetingView: View {
                 Circle()
                     .strokeBorder(lineWidth: 24)
 
-                HStack {
-                    Text("Speaker 1 of 3")
-
-                    Spacer()
-
-                    Button(action: {}, label: {
-                        Image(systemName: "forward.fill")
-                    })
-                    .accessibilityLabel("Next speaker")
-                }
+                MeetingFooterView(
+                    speakers: scrumTimer.speakers,
+                    skipAction: scrumTimer.skipSpeaker)
             }
         }
         .padding()
