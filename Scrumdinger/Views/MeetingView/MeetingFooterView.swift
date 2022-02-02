b2d2bb3ca5c5481b56824c94f005592bed45e458
Florian Bruder <florian.bruder@posteo.de>
2022-02-02 21:23:25 +0100

    Add MeetingFooterView.swift

---
+++ Scrumdinger/Views/MeetingView/MeetingFooterView.swift
@@ -0,0 +1,24 @@
+//
+//  MeetingFooterView.swift
+//  Scrumdinger
+//
+//  Created by Florian Bruder on 02.02.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct MeetingFooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct MeetingFooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingFooterView()
+    }
+}
