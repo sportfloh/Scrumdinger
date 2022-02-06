2e361946f2105baa90727cfe59a2e9078e5319c5
Florian Bruder <florian.bruder@posteo.de>
2022-02-06 19:40:36 +0100

    Implement ErrorView

--- Scrumdinger/Views/ErrorView.swift
+++ Scrumdinger/Views/ErrorView.swift
@@ -10,15 +10,51 @@ import SwiftUI
 // MARK: -
 
 struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    @Environment(\.dismiss) private var dismiss
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("An error has occurred!")
+                    .font(.title)
+                    .padding(.bottom)
+                Text(errorWrapper.error.localizedDescription)
+                    .font(.headline)
+                Text(errorWrapper.guidance)
+                    .font(.caption)
+                    .padding(.top)
+                Spacer()
+            }
+            .padding()
+            .background(.ultraThinMaterial)
+            .cornerRadius(16)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Dismiss") {
+                        dismiss()
+                    }
+                }
+            }
+        }
     }
 }
 
 // MARK: -
 
 struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(
+            error: SampleError.errorRequired,
+            guidance: "You can safely ignore this error.")
+    }
+
     static var previews: some View {
-        ErrorView()
+        ErrorView(errorWrapper: wrapper)
     }
 }
