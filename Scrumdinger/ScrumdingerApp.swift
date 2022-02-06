1b1824c6df1e5d25707d28e9301e6fdc6516d220
Florian Bruder <florian.bruder@posteo.de>
2022-02-06 19:41:18 +0100

    Present the ErrorView on failure and load sample data

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -12,6 +12,7 @@ import SwiftUI
 @main
 struct ScrumdingerApp: App {
     @StateObject private var store = ScrumStore()
+    @State private var errorWrapper: ErrorWrapper?
 
     var body: some Scene {
         WindowGroup {
@@ -21,7 +22,7 @@ struct ScrumdingerApp: App {
                         do {
                             try await ScrumStore.save(scrums: store.scrums)
                         } catch {
-                            fatalError("Error saving scrums.")
+                            errorWrapper = ErrorWrapper(error: error, guidance: "Try again later.")
                         }
                     }
                 }
@@ -30,9 +31,18 @@ struct ScrumdingerApp: App {
                 do {
                     store.scrums = try await ScrumStore.load()
                 } catch {
-                    fatalError("Error loading scrums.")
+                    errorWrapper = ErrorWrapper(error: error, guidance: "Scrumdinger will load sample data and continue.")
                 }
             }
+            .sheet(
+                item: $errorWrapper,
+                onDismiss: {
+                    store.scrums = DailyScrum.sampleData
+                },
+                content: { wrapper in
+                    ErrorView(errorWrapper: wrapper)
+
+                })
         }
     }
 }
