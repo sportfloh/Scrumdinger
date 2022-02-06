b0e064e3ba56ea33390d1fe781e135c9aacb9360
Florian Bruder <florian.bruder@posteo.de>
2022-02-06 19:17:43 +0100

    Add ErrorView.swift

---
+++ Scrumdinger/Views/ErrorView.swift
@@ -0,0 +1,24 @@
+//
+//  ErrorView.swift
+//  Scrumdinger
+//
+//  Created by Florian Bruder on 06.02.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorView()
+    }
+}
