46274f10242868616fd6a75b2048d2cb63d7ddba
Florian Bruder <florian.bruder@posteo.de>
2022-02-07 09:37:08 +0100

    Add MeetingTimerView.swift

---
+++ Scrumdinger/Views/MeetingView/MeetingTimerView.swift
@@ -0,0 +1,24 @@
+//
+//  MeetingTimerView.swift
+//  Scrumdinger
+//
+//  Created by Florian Bruder on 07.02.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct MeetingTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct MeetingTimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingTimerView()
+    }
+}
