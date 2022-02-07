84af6dacb659e82caa4edb87fd144d4f13801d75
Florian Bruder <florian.bruder@posteo.de>
2022-02-07 20:35:01 +0100

    Add test for History type

--- ScrumdingerTests/HistoryTests.swift
+++ ScrumdingerTests/HistoryTests.swift
@@ -13,3 +13,35 @@ import XCTest
 final class HistoryTestCase: XCTestCase {}
 
 // MARK: -
+
+extension HistoryTestCase {
+    func testHistory() {
+        let id = UUID()
+        let date = Date()
+        let attendees = [
+            DailyScrum.Attendee(name: "Jon"),
+            DailyScrum.Attendee(name: "Darla"),
+            DailyScrum.Attendee(name: "Luis")
+        ]
+        let lengthInMinutes = 10
+        let transcript = "This is a test transcript."
+        let attendeeString = ListFormatter.localizedString(
+            byJoining: attendees.map { $0.name }
+        )
+
+        let history = History(
+            id: id,
+            date: date,
+            attendees: attendees,
+            lengthInMinutes: lengthInMinutes,
+            transcript: transcript
+        )
+
+        XCTAssertEqual(id, history.id)
+        XCTAssertEqual(date, history.date)
+        XCTAssertEqual(attendees, history.attendees)
+        XCTAssertEqual(lengthInMinutes, history.lengthInMinutes)
+        XCTAssertEqual(transcript, history.transcript)
+        XCTAssertEqual(attendeeString, history.attendeeString)
+    }
+}
