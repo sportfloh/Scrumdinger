fded5e937fc0fd416f968dfe80981a96e759810c
Florian Bruder <florian.bruder@posteo.de>
2022-02-07 12:48:03 +0100

    Implement HistoryView

--- Scrumdinger/Views/MeetingView/HistoryView.swift .swift
+++ Scrumdinger/Views/MeetingView/HistoryView.swift .swift
@@ -10,15 +10,49 @@ import SwiftUI
 // MARK: -
 
 struct HistoryView: View {
+    let history: History
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading) {
+                Divider()
+                    .padding(.bottom)
+                Text("Attendees")
+                    .font(.headline)
+                Text(history.attendeeString)
+                if let transcript = history.transcript {
+                    Text("Transcript")
+                        .font(.headline)
+                        .padding(.top)
+                    Text(transcript)
+                }
+            }
+        }
+        .navigationTitle(Text(history.date, style: .date))
+        .padding()
+    }
+}
+
+// MARK: -
+
+extension History {
+    var attendeeString: String {
+        ListFormatter.localizedString(byJoining: attendees.map { $0.name })
     }
 }
 
 // MARK: -
 
 struct HistoryView_Previews: PreviewProvider {
+    static var history: History {
+        History(attendees: [
+            DailyScrum.Attendee(name: "Jon"), DailyScrum.Attendee(name: "Darla"), DailyScrum.Attendee(name: "Luis")
+        ], lengthInMinutes: 10,
+        // swiftlint:disable:next line_length
+        transcript: "Darla, would you like to start today? Sure, yesterday I reviewed Luis' PR and met with the design team to finalize the UI...")
+    }
+
     static var previews: some View {
-        HistoryView()
+        HistoryView(history: history)
     }
 }
