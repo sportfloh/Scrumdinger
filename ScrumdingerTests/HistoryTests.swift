acdd25f3d502e084656978b11d5496a1f7cf58a5
Florian Bruder <florian.bruder@posteo.de>
2022-02-07 20:02:46 +0100

    Add HistoryTests.swift

---
+++ ScrumdingerTests/HistoryTests.swift
@@ -0,0 +1,15 @@
+//
+//  HistoryTests.swift
+//  ScrumdingerTests
+//
+//  Created by Florian Bruder on 07.02.22.
+//
+
+@testable import Scrumdinger
+import XCTest
+
+// MARK: -
+
+final class HistoryTestCase: XCTestCase {}
+
+// MARK: -
